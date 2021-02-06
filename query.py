38bc8ea41edf903054249d7506c5d60cd42d125c
xspure666 <1002569591@qq.com>
2021-02-06 11:26:49 +0800

    2021-02-06

--- query.py
+++ query.py
@@ -6,6 +6,7 @@ import codecs
 import csv
 
 
+# 查询数据
 def conn_mysql(level, start, end):
     sql = open('sql/sql.txt', 'r', encoding='utf-8').read()
     try:
--- start_py.sh
+++ start_py.sh
@@ -1,3 +1,4 @@
+# 自动化执行
 cd /root/mysql
 
 python3 main.py >> /root/mysql/send.log 2>&1 &
